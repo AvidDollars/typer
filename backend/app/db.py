e292cda75053d8712b04ff1759f3492c08d1c11d
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-06-27 17:53:25 +0200

    added file for interaction with a database

---
+++ backend/app/db.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+
+
+class Database:
+
+    def __init__(self, db_url: str, environment: str):
+        self._engine = create_async_engine(
+            db_url,
+            echo=True if environment == "development" else False,
+            future=True
+        )
+
+    async def initialize(self):
+        async with self._engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+
+    async def session(self):
+        async_session = sessionmaker(
+            self._engine,
+            class_=AsyncSession,
+            expire_on_commit=False
+        )
+
+        with async_session() as session:
+            yield session
+
