f48d6025b827daa91114ba7ef9e14acdc5c67775
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-06-28 13:07:07 +0200

    app instance factory modified and moved to main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,18 @@
-from .app_instance import create_app
+from fastapi import FastAPI
+
+from .containers import Container
 from .routers import router
 
+
+def create_app() -> FastAPI:
+
+    app = FastAPI()
+    app.include_router(router)
+
+    container = Container()
+    app.container = container
+
+    return app
+
+
 app = create_app()
-app.include_router(router)
