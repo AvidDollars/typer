fb6066c4dad7a788bbb0a6326a2726be72b36dff
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-06-28 21:41:01 +0200

    updated constants

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -4,3 +4,7 @@ APP_DIR = path.dirname(path.abspath(__file__))
 LOG_DIR = path.abspath("logs")
 
 CONFIG_FILE_NAME = ".env.yml"
+
+# SENDING EMAILS
+EMAIL_TEMPLATE_DIR = path.abspath("email_templates")
+REGISTER_TEMPLATE_FILE_NAME = "registration.html"
