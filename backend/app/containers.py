d180893790beff96a626774a16eaab601c84978b
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-06-28 13:10:58 +0200

    added wiring config

--- backend/app/containers.py
+++ backend/app/containers.py
@@ -1,12 +1,12 @@
 from os import path
 
-from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Configuration, Singleton, Resource
+from dependency_injector.containers import DeclarativeContainer, WiringConfiguration
+from dependency_injector.providers import Configuration, Singleton, Resource, Factory
 
-from .constants import APP_DIR, CONFIG_FILE_NAME
+from .constants import APP_DIR, CONFIG_FILE_NAME, LOG_DIR
 from .db import Database
 from .logger import get_logger
-from .constants import LOG_DIR
+from .services import EmailService
 
 
 class Container(DeclarativeContainer):
@@ -14,8 +14,13 @@ class Container(DeclarativeContainer):
     dependency injection container
     """
 
+    # WIRING CONFIG
+    wiring_config = WiringConfiguration(packages=[".routers"])
+
+    # CONFIG
     config = Configuration(yaml_files=[path.join(APP_DIR, CONFIG_FILE_NAME)])
 
+    # LOGGER
     logger = Resource(
         get_logger,
         level=config.logging["level"],
@@ -23,9 +28,16 @@ class Container(DeclarativeContainer):
         out_folder=LOG_DIR
     )
 
+    # DATABASE
     db = Singleton(
         Database,
         db_url=config.db_url,
         environment=config.environment
     )
 
+    # SERVICES
+    email_service = Factory(
+        EmailService,
+        email_config=config.smtp,
+        logger=logger
+    )
