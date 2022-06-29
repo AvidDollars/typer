caf291922b50f74b85b70b6dcf40c2c0b498b5a1
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-06-29 11:44:27 +0200

    added regex for email validation

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,10 +1,15 @@
 from os import path
 
+# DIRECTORIES
 APP_DIR = path.dirname(path.abspath(__file__))
 LOG_DIR = path.abspath("logs")
+EMAIL_TEMPLATE_DIR = path.abspath("email_templates")
 
+# CONFIG
 CONFIG_FILE_NAME = ".env.yml"
 
-# SENDING EMAILS
-EMAIL_TEMPLATE_DIR = path.abspath("email_templates")
+# WILL CATCH MOST OF THE INVALID EMAILS
+EMAIL_VALIDATOR_REGEX = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+# EMAIL TEMPLATES
 REGISTER_TEMPLATE_FILE_NAME = "registration.html"
