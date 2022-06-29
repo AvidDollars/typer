2064a29f17caf0eeb426ce1b97ac7a1ff1a99287
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-06-29 11:33:24 +0200

    created function for logging server errors and for handling Pydantic validation errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
 from .containers import Container
+from .middleware.exception_handlers import log_server_errors, validation_error_handler
 from .routers import router
 
 
@@ -8,6 +10,9 @@ def create_app() -> FastAPI:
 
     app = FastAPI()
     app.include_router(router)
+
+    app.middleware("http")(log_server_errors)
+    app.exception_handler(RequestValidationError)(validation_error_handler)
 
     container = Container()
     app.container = container
---
+++ backend/app/middleware/exception_handlers.py
@@ -0,0 +1,28 @@
+from dependency_injector.wiring import Provide, inject
+from fastapi import Request, status, Response
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+from ..containers import Container
+
+
+@inject
+async def log_server_errors(
+        request: Request,
+        call_next,
+        logger=Provide[Container.logger]
+):
+    try:
+        response = await call_next(request)
+        return response
+
+    except Exception as ex:
+        logger.warning(f"exception: {ex}\n", exc_info=True)
+        return Response("internal server error", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def validation_error_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"error": exc.errors()[0]["msg"]}
+    )
