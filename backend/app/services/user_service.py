a446d2c0d14cbc047e3c9e0552f50697be1bf7b7
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-06-30 23:53:39 +0200

    added 'save_user' method to 'UserService'

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,9 +1,22 @@
-from ..db import Database
-
+from ..models.user import UserDb, UserIn
+from . import hashing_service
 
 __all__ = ("UserService", )
 
 
 class UserService:
-    def __init__(self, db: Database):
-        self.db = db
+    def __init__(
+            self, *,
+            hashing_service: hashing_service.AbstractHashingService,
+            repository
+    ):
+        self.hashing_service = hashing_service
+        self.repository = repository
+
+    async def save_user(self, user: UserIn):
+        hashed_password = self.hashing_service.hash(user.password)
+        user.password = hashed_password
+        userDb = UserDb.from_orm(user)
+
+        activation_link = await self.repository.save_user(userDb)
+        return activation_link
