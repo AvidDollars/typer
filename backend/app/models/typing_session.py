9a98a6972737225a228af76c30fdb305a48782cb
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-07-07 16:06:08 +0200

    models for typing session updated

--- backend/app/models/typing_session.py
+++ backend/app/models/typing_session.py
@@ -10,15 +10,19 @@ from sqlmodel import SQLModel, Field
 class TypingSessionBase(SQLModel):
     duration_in_seconds: int = Field(nullable=False)
     text_id: UUID4 = Field(foreign_key="texts.id", nullable=False)
-    user_id: UUID4 = Field(foreign_key="users.id", nullable=False)
-    stats: dict = Field(sa_column=Column(JSON), nullable=False)
+    stats: dict = Field(sa_column=Column(JSON), nullable=False)  # TODO: created more explicit model for stats
 
 
-class TypingSessionDb(TypingSessionBase, table=True):
+class TypingSessionIn(TypingSessionBase):
+    ...
+
+
+class TypingSessionDb(TypingSessionIn, table=True):
 
     __tablename__ = "typing_sessions"
 
     id: UUID4 | None = Field(primary_key=True, nullable=False, index=True, default_factory=uuid4)
+    user_id: UUID4 = Field(foreign_key="users.id", nullable=False)
     created_at: datetime = Field(nullable=False, default_factory=datetime.now)
 
 
