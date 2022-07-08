d763504181bfad56b1c5a42154a399559702320e
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-07-08 17:48:56 +0200

    implemented functionality for updating rating of a text

--- backend/app/routers/text_rating.py
+++ backend/app/routers/text_rating.py
@@ -2,27 +2,36 @@ from dependency_injector.wiring import Provide
 from dependency_injector.wiring import inject
 from fastapi import APIRouter, Depends, status
 from fastapi.requests import Request
+from fastapi.responses import Response
 
 from ..containers import Container
-from ..models.text_rating import TextRatingIn, TextRatingDb
+from ..models.text_rating import TextRatingIn, TextRatingDb, TextRatingUpdate
 from ..services import TextRatingService
 from ..services.auth_service import required_authentication
 
 router = APIRouter(
-    prefix="/text-ratings", tags=["Text Ratings"]
+    prefix="/text-ratings", tags=["Text Ratings"], dependencies=[Depends(required_authentication)]
 )
 
 
-@router.post(
-    "",
-    dependencies=[Depends(required_authentication)],
-    status_code=status.HTTP_201_CREATED,
-)
+@router.post("", status_code=status.HTTP_201_CREATED)
 @inject
 async def add_text_rating(
         request: Request,
-        text: TextRatingIn,
+        text_rating: TextRatingIn,
         text_rating_service: TextRatingService = Depends(Provide[Container.text_rating_service])
 ):
-    text_rating = TextRatingDb(**text.dict(), rated_by=request.user_id)
+    text_rating = TextRatingDb(**text_rating.dict(), rated_by=request.user_id)
     await text_rating_service.insert_text_rating(text_rating=text_rating)
+
+
+@router.patch("")
+@inject
+async def update_text_rating(
+    request: Request,
+    text_rating: TextRatingUpdate,
+    text_rating_service: TextRatingService = Depends(Provide[Container.text_rating_service])
+):
+    text_rating = TextRatingDb(**text_rating.dict(), rated_by=request.user_id)
+    await text_rating_service.update_text_rating(text_rating=text_rating)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- backend/app/services/text_rating_service.py
+++ backend/app/services/text_rating_service.py
@@ -1,5 +1,5 @@
 from ..repositories import CrudOperations
-from ..models.text_rating import TextRatingDb
+from ..models.text_rating import TextRatingDb, TextRatingUpdate
 from sqlalchemy.exc import IntegrityError
 from ..constants import UNIQUE_CONSTRAINT_VIOLATED
 from fastapi import HTTPException, status
@@ -26,3 +26,13 @@ class TextRatingService:
             else:
                 raise  # unexpected error... will be logged
 
+    async def update_text_rating(self, *, text_rating: TextRatingDb):
+        rows_affected = await self.repository.update_resource(
+            TextRatingDb,
+            resource_id=text_rating.id,
+            _filter=TextRatingDb.rated_by == text_rating.rated_by,
+            rating=text_rating.rating
+        )
+
+        if not rows_affected:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="text rating not found")
