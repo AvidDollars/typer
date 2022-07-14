7fb7a7e690bbf114581b14f334b1737b0d895aae
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-07-14 14:39:38 +0200

    app factory moved to its dedicated module

---
+++ backend/app/main/app_factory.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+
+from ..containers import Container
+from ..middleware.exception_handlers import log_server_errors, validation_error_handler
+from ..routers import router
+
+__all__ = "create_app",
+
+
+def create_app() -> FastAPI:
+    container = Container()
+    config = container.config()
+
+    app = FastAPI(title=config["project_name"])
+    app.container = container
+    app.include_router(router)
+    app.middleware("http")(log_server_errors)
+    app.exception_handler(RequestValidationError)(validation_error_handler)
+
+    return app
