feb3386cc9253b2f3077bb932d10a6b7f5b436be
Dimitrij Kolnicenko <dimitrij.kol@protonmail.com>
2022-07-15 21:01:36 +0200

    modified tests for /register/ endpoint

--- backend/app/tests/test_routes/test_user_registration.py
+++ backend/app/tests/test_routes/test_user_registration.py
@@ -8,23 +8,27 @@ async def test_invalid_arguments_for_user_registration(async_client):
 
     # PROVIDED NAME IS TOO SHORT
     name_is_short = dict(name="no", email="thisis.valid@email.com", password="abc123DEF")
-    response = await async_client.post("/register/", data=name_is_short)
+    response = await async_client.post("/register/", json=name_is_short)
     assert response.status_code == 400
+    assert response.json()["error"] == "value_error"
 
     # PROVIDED NAME HAS INVALID SYMBOL
     name_has_invalid_symbol = dict(name="inv@lid", email="thisis.valid@email.com", password="abc123DEF")
-    response = await async_client.post("/register/", data=name_has_invalid_symbol)
+    response = await async_client.post("/register/", json=name_has_invalid_symbol)
     assert response.status_code == 400
+    assert response.json()["error"] == "value_error"
 
     # EMAIL ADDRESS IS NOT VALID
     email_is_invalid = dict(name="John", email="inv@lid", password="abc123DEF")
-    response = await async_client.post("/register/", data=email_is_invalid)
+    response = await async_client.post("/register/", json=email_is_invalid)
     assert response.status_code == 400
+    assert response.json()["error"] == "value_error"
 
     # PASSWORD IS NOT STRONG ENOUGH
     email_is_invalid = dict(name="John", email="thisis.valid@email.com", password="w3ak_password")
-    response = await async_client.post("/register/", data=email_is_invalid)
+    response = await async_client.post("/register/", json=email_is_invalid)
     assert response.status_code == 400
+    assert response.json()["error"] == "value_error"
 
 
 @pytest.mark.asyncio
