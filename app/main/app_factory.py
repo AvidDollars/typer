354d72857786dfb5a45cba8e6fba15477bf13047
Dimitrij Kolničenko <dimitrij.kolnicenko@accenture.com>
2022-10-12 13:23:41 +0200

    .

--- app/main/app_factory.py
+++ app/main/app_factory.py
@@ -12,7 +12,7 @@ def create_app() -> FastAPI:
     container = Container()
     config = container.config()
 
-    app = FastAPI(title=config["project_name"])
+    app = FastAPI(title=config.get("project_name") or "default name")
     app.container = container
     app.include_router(router)
     app.middleware("http")(log_server_errors)
