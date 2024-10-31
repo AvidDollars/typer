a87c23497d5e89d53fa0fccbbf99516cd5ab1af1
AvidDollars <dima.kol@seznam.cz>
2024-11-01 00:39:59 +0100

    add CORS to backend to allow communication with frontend

--- backend/app/main/app_factory.py
+++ backend/app/main/app_factory.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 from containers import Container
 from middleware.exception_handlers import log_server_errors, validation_error_handler
@@ -17,5 +18,12 @@ def create_app() -> FastAPI:
     app.include_router(router)
     app.middleware("http")(log_server_errors)
     app.exception_handler(RequestValidationError)(validation_error_handler)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
 
     return app
