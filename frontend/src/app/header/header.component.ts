926b7ff143f18160c73d1bd5512a2a8d53beb29a
AvidDollars <dima.kol@seznam.cz>
2025-03-14 14:53:23 +0100

    finish html + css of header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TitleComponent } from './title/title.component';
+import { UserSpaceComponent } from './user-space/user-space.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    TitleComponent, UserSpaceComponent,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-
 }
