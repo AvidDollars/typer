4ea4e17f19082ceb551d3d540272664488742600
AvidDollars <dima.kol@seznam.cz>
2025-04-03 13:03:30 +0200

    navigate to "/" when clicking on title in the header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TitleComponent } from './title/title.component';
 import { UserSpaceComponent } from './user-space/user-space.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    TitleComponent, UserSpaceComponent,
+    TitleComponent, UserSpaceComponent, RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
