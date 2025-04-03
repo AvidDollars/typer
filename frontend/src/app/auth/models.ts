6d13b81294aef2e7fcd09827c68c0f0a8d6c8068
AvidDollars <dima.kol@seznam.cz>
2025-04-03 12:27:58 +0200

    remove redundant models

--- frontend/src/app/auth/models.ts
+++ frontend/src/app/auth/models.ts
@@ -15,21 +15,3 @@ export class TokenPayload {
         this.expiration = exp;
     }
 };
-
-/**
- * User credentials for "/register" and "/login" routes.
- */
-export interface UserCredentials {
-    name?: string;
-    email?: string;
-    password: string;
-};
-
-/**
- * Represents exit status of login operation.
- */
-export interface LoginOperation {
-    status: "logged" | "errored";
-    errorMessage?: string;
-    statusCode?: number;
-};
