0d6405e4c09773532e7857d4dec9940f81521580
AvidDollars <dima.kol@seznam.cz>
2025-04-18 19:51:17 +0200

    refactor code and improve logic for typing session

--- frontend/src/app/main-area/main-area.component.ts
+++ frontend/src/app/main-area/main-area.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, effect, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
-import { filter, map, scan, takeWhile, finalize, timer, interval, Subject, fromEvent, takeUntil, startWith, concatMap } from 'rxjs';
+import { ChangeDetectionStrategy, Component, effect, ElementRef, inject, OnInit, viewChild } from '@angular/core';
+import { filter, map, scan, takeWhile, finalize, interval, Subject, fromEvent, takeUntil, startWith, concatMap, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { TextLoaderService } from './text-loader.service';
-import { discardIrrelevantKeys, extractKey } from './utils';
+import { discardIrrelevantKeys, extractKey, SessionState } from './utils';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { ConfettiService } from '../confetti/confetti.service';
 
@@ -12,7 +12,9 @@ import { ConfettiService } from '../confetti/confetti.service';
   templateUrl: './main-area.component.html',
   styleUrl: './main-area.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { // TODO: improve layout for small screens
+
+  // TODO: improve layout for small screens
+  host: {
     class: `
       overflow-scroll no-scrollbar
       relative bg-light rounded-xl p-2
@@ -30,14 +32,27 @@ export class MainAreaComponent implements OnInit {
   confettiService = inject(ConfettiService); // confetti at the end of typing session
   textLoaderService = inject(TextLoaderService);
   loadedText = this.textLoaderService.text;
-  characterArray: string[] = [];
-  finishedTyping = signal(false);
-
+  sessionState = new SessionState();
   hostElement = inject(ElementRef).nativeElement as HTMLElement;
   textareaElement = viewChild<ElementRef<HTMLTextAreaElement>>("textarea");
 
+  constructor() {
+    // TODO: try to create better solution which does not involve using "effect"
+    effect(() => { // new text -> new character array
+      let text = this.loadedText() ?? "";
+      this.sessionState.loadText(text);
+    });
+  }
+
+  ngOnInit() {
+    this.hostElement.scrollTo({ top: 0, behavior: "smooth" });
+
+    // TODO: autoscrolling
+    //interval(50).pipe(delay(2_000)).subscribe(val => this.host.scrollTo({top: val / 5, behavior: "smooth"}))
+  }
+
   initTextarea$ = toObservable(this.textareaElement).pipe(
-    filter(value => value != undefined),
+    filter(Boolean),
     map(textarea => {
       const element = textarea.nativeElement;
       element.focus(); // to retrieve lost focus on textarea when going to a different page
@@ -47,70 +62,38 @@ export class MainAreaComponent implements OnInit {
 
   // TYPING SESSION TRIGGERS
   startTyping$ = new Subject<void>();
-  endTyping$ = new Subject<void>();
-
-  typingStream$ = fromEvent<KeyboardEvent>(this.hostElement, "keydown").pipe(
-    filter(discardIrrelevantKeys), // to filter out "Shift" / "CapsLock" / etc...
-    map(extractKey),
-    scan((acc, key) => {
-      let index = (key === "Backspace") ? --acc[0] : ++acc[0];
-      index = (index < 0) ? -1 : index; // min index: -1
-      return [index, key] as [number, string];
-    }, [-1, ""] as [number, string]),
-    takeWhile(([index, _key]) => index < this.characterArray.length - 1), // TODO: you must add extra char to finish the session
-    map(([index, char]) => {
-      return [index, char, this.characterArray[index] === char] as [number, string, boolean];
-    }),
-    scan((error_counter, [_index, key, isCorrectKey]) => {
-      if (!isCorrectKey && key !== "Backspace") {
-        error_counter.set(key, (error_counter.get(key) ?? 0) + 1)
-      };
-      return error_counter;
-    }, new Map(this.characterArray.map(char => [char, 0]))),
-    finalize(() => {
-      this.endTyping$.next();
-      this.finishedTyping.set(true);
-
-      timer(200).subscribe(() => {
-        this.confettiService.celebrate();
-      });
-    }),
+  sessionFinished$ = new Subject<boolean>();
+  celebrateFinish$ = this.sessionFinished$.pipe(
+    tap(() => this.confettiService.celebrate())
   );
-
-  // TYPING
-  typing$ = this.startTyping$.pipe(
-    concatMap(() => this.typingStream$),
-  );
-
-  // CLOCK
-  clock$ = this.startTyping$.pipe(
-    concatMap(() => interval(10).pipe(
-      map(value => value + 1),
-      startWith(0),
-      takeUntil(this.endTyping$)
-    )),
-  );
-
-  constructor() {
-    effect(() => { // new text -> new character array
-      let text = this.loadedText() ?? "";
-      this.characterArray = [...text];
-    });
-  }
-
-  ngOnInit() {
-    this.hostElement.scrollTo({ top: 0, behavior: "smooth" });
-    this.typing$.subscribe(v => console.log("v: ", v));
-
-    // TODO: autoscrolling
-    //interval(50).pipe(delay(2_000)).subscribe(val => this.host.scrollTo({top: val / 5, behavior: "smooth"}))
-  }
 
   /**
    * Initializes typing session and immediately removes itself as event listener.
    */
   initTypingSession = () => {
     this.startTyping$.next();
-    this.textareaElement()?.nativeElement.removeEventListener("input", this.initTypingSession)
+    this.textareaElement()?.nativeElement.removeEventListener("input", this.initTypingSession);
   }
+
+  // TYPING
+  typing$ = this.startTyping$.pipe(
+    startWith(null), // to start the stream on 1st keystroke
+    concatMap(() => fromEvent<KeyboardEvent>(this.hostElement, "keydown")),
+    filter(discardIrrelevantKeys), // to filter out "Shift" / "CapsLock" / etc...
+    map(extractKey),
+    scan((state, char) => state.updateState(char), this.sessionState),
+    takeWhile(state => !state.isFinished),
+    finalize(() => this.sessionFinished$.next(true)),
+  );
+
+  // CLOCK
+  clock$ = this.startTyping$.pipe(
+    concatMap(() => interval(1000).pipe(
+      // Time between calls in operators is bigger than value specified in "interval" observable.
+      // TODO: implement clock which will correct itself, so that "duration of session = elapsed time on clock"
+      map(value => value + 1),
+      startWith(0),
+      takeUntil(this.sessionFinished$),
+    )),
+  );
 }
