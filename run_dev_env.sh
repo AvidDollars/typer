f99a718f5f6585199f40fd877bdb28179d97a600
AvidDollars <dima.kol@seznam.cz>
2025-05-03 11:57:49 +0200

    improve script for running DEV env -> code refactor + missing dependencies check

--- run_dev_env.sh
+++ run_dev_env.sh
@@ -1,25 +1,38 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -e # exit immediately on error
 
 # generates key + cert in shared folder
 # TO BE USED ONLY IN DEV ENV!
-DESTINATION="shared"
+DESTINATION="./shared"
 KEY=$DESTINATION/dev_cert.pem
 CERT=$DESTINATION/dev_key.pem
-DATETIME=$(date "+%Y-%m-%dt%H_%M_%S")
 
-if [[ -f $KEY && -f $CERT ]]; then
-    echo "'$KEY' and  '$CERT' already exist. Skipping the key + cert creation."
-else
-    echo "Creating '$KEY' and  '$CERT'."
-    openssl req -x509 \
-        -newkey rsa:4096 -nodes \
-        -out $KEY \
-        -keyout $CERT \
-        -days 365 \
-        -subj "/CN=localhost"
-fi
+function exit_on_missing_dependency() {
+    [[ $# -eq 0 ]] && echo "no argument provided" && exit 1
 
+    for dependency in "$@"; do
+        command -v $dependency >/dev/null 2>&1 || \
+        (echo "'$dependency' is not installed" && exit 1)
+    done
+}
+
+# creates self-signed SSL cert if missing
+function init_ssl_cert() {
+    if [[ -f $KEY && -f $CERT ]]; then
+        echo "'$KEY' and '$CERT' already exist. Skipping the creation of key and cert."
+    else
+        echo "Creating '$KEY' and  '$CERT'."
+        openssl req -x509 -nodes \
+            -newkey rsa:4096 \
+            -out $KEY \
+            -keyout $CERT \
+            -days 365 \
+            -subj "/CN=localhost"
+    fi
+}
+
+exit_on_missing_dependency openssl docker docker-compose
+init_ssl_cert
 # TODO: create better solution for running migration on the start of backend
 # running migrations (sleep is needed in order to connect to DB)
 docker compose -f dev.docker-compose.yaml up --detach && sleep 1 && \
